c57a035e750e2e57d72030a613b8ec60b4417a2b
Tran Thuy Vy <ttv.thuyvy.2112@gmail.com>
2022-01-15 08:48:24 +0700

    finish

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from pygame import time as time
 from pygame.event import get
 from pygame.constants import QUIT, K_SPACE, KEYDOWN
 
-
 def init_game():
     background_x, background_y = 0, 0
     dinosaur_x, dinosaur_y = 100, 360
@@ -19,11 +18,9 @@ def init_game():
     bool_s2 = True
     return bool_s2, background_x, background_y, dinosaur_x, dinosaur_y, obj_x, obj_y, x_velocity, y_velocity, score, pausing
 
-
 def write(x, y, text, font):
     a = font.render(text, True, RED)
     screen.blit(a, (x, y))
-
 
 def velocity_background(background_x):
     screen.blit(background, (background_x, background_y))
@@ -33,36 +30,30 @@ def velocity_background(background_x):
         background_x = 0
     return background_x
 
-
 def velocity_obj(obj_x, i):
-    # nó tương tự velocity_tree lúc trước nhưng giờ ghi object để dùng chung cho đám mây
     obj_rect = screen.blit(obj[i], (obj_x, obj_y))
     obj_x -= x_velocity
-    if obj_x <= -100:  # -100 là tọa độ biến mất của object, nếu để nó lớn hơn nhiều quá thì nó sẽ giống như biến mất chứ
-        # không phải chạy qua
-        obj_x = 1100  # 1000 là tọa độ xuất hiện của object, nếu để nó nhỏ hơn nhiều quá thì nó sẽ giống như bổng nhiên
-        # xuất hiện không phải chạy ra (xuất hiện bên phải ngoài màn hình background)
-        return obj_rect, obj_x, True  # true/ false là để cho step
+    if obj_x <= -100:
+        obj_x = 1100
+        return obj_rect, obj_x, True
     return obj_rect, obj_x, False
-
 
 def velocity_dinosaur(dinosaur_y, jump, up):
     up += 1
-    if up <= 10 or jump:  # nếu vòng while đã chạy 11 lần hoặc con khủng long đang nhảy thì tọa độ xuất hiện của con khủng long sẽ bình thường
+    if up <= 10 or jump:
         dinosaur_rect = screen.blit(dinosaur, (dinosaur_x, dinosaur_y))
-    else:  # nếu nó không nhảy mà chạy thì cứ 10 lượt vòng while nó sẽ nhảy lên trên 20 cm
+    else:
         dinosaur_rect = screen.blit(dinosaur, (dinosaur_x, dinosaur_y - 20))
-        if up == 20:  # sau 10 lượt vòng while nữa nó sẽ trở lại mặt đất up sẽ reset về 0
+        if up == 20:
             up = 0
-    if 360 >= dinosaur_y >= 100 and jump:  # nhảy lên
+    if 360 >= dinosaur_y >= 100 and jump:
         dinosaur_y -= y_velocity
     else:
         jump = False
-    if dinosaur_y < 360 and jump == False:  # nhảy xuống
+    if dinosaur_y < 360 and jump == False:
         dinosaur_y += y_velocity
-        up = 0  # vì lúc này còn nhảy nhưng jump đã về false nên phải set up về 0 để nó k bị nhảy tưng tưng trên không
+        up = 0
     return dinosaur_rect, dinosaur_y, jump, up
-
 
 def colliderect_rect():
     if dinosaur_rect[0] + dinosaur_rect[2] < obj_rect[0] or obj_rect[0] + obj_rect[2] < dinosaur_rect[0]:
@@ -85,24 +76,18 @@ def random_obj(x_velocity, score):
     score += 1
     if score % 3 == 0 and score <= 27:
         x_velocity += 1
-    # Mỗi lần obj đạt mốc x*3 thì tốc độ tăng 1 nhưng tới 100đ tốc độ không tăng nữa
-    # Vì tới 100đ là tốc độ tăng gấp 10 lần rồi rất nhanh rồi
     return rand_obj, x_velocity, score
-
 
 if __name__ == '__main__':
     init()
-    screen = display.set_mode((1000, 500))  # Độ dài, rộng của màn hình
-    display.set_caption('DAMH_N19DCCN112_N19DCCN231')  # Tên của cửa sổ
+    screen = display.set_mode((1000, 500))
+    display.set_caption('DAMH_N19DCCN112_N19DCCN231')
     bool_s2, background_x, background_y, dinosaur_x, dinosaur_y, obj_x, obj_y, x_velocity, y_velocity, score, pausing = init_game()
-    # bool_s2: s2 là sound2_ âm kết thúc, = true là chưa kêu lần nào. kêu 1 lần nó sẽ về false
 
     WHITE = (255, 255, 255)
     RED = (255, 0, 0)
 
     step, rand_obj, rand2 = 0, 1, 1
-    # step là bước chuyển của object hiện tại đang là 1200 (là một bước) = 1000 chiều ngang của backround (X)
-    # + 200 (là rìa xuất hiện và biến mất đã nói ở hàm velocity_obj
 
     font = SysFont('Times New Roman', 20)
     font1 = SysFont('Times New Roman', 40)
@@ -123,11 +108,9 @@ if __name__ == '__main__':
     FPS = 60
     clock = time.Clock()
 
-    jump = False  # Nhảy cao
+    jump = False
     running = True
-    up = 0  # Nhảy tưng tưng (chạy)
-    # k để kiểu bool vì 1 vòng while đã nhảy thì rất nhanh rối mất, nên cho nó đếm mỗi vòng while
-    # Nếu nó đặt mốc 10 sẽ nhảy lên 1 lần.
+    up = 0
     speed = 1
 
     while running:
@@ -143,28 +126,20 @@ if __name__ == '__main__':
 
         if step:
             rand_obj, x_velocity, score = random_obj(x_velocity, score)
-            # random hình xuất hiện. nếu k có if step nó sẽ xuất hiện lung tung
-            # xóa if để xem thử sẽ biết
+
         if rand_obj < 3:
             obj_y = 320
-            # Nhỏ hơn 3 là cây tung độ xuất hiện của nó sẽ là 320
         else:
             if 1100 >= obj_x >= 1000:
                 obj_y = randint(150, 320)
-            # từ 3-5 sẽ là mây mây sẽ được random tọa độ
-            # if 1000 tới 1100 vì ở đây object sẽ chưa xuất hiện có random cũng không làm hình xuất hiện lung tung
-            # khoảng này có thể nhỏ hơn 1050-1100 hay 1000- 1050 nói chung trong khoảng object chưa xuất hiên
-        obj_rect, obj_x, step = velocity_obj(obj_x, rand_obj)
 
+        obj_rect, obj_x, step = velocity_obj(obj_x, rand_obj)
         dinosaur_rect, dinosaur_y, jump, up = velocity_dinosaur(dinosaur_y, jump, up)
         pausing, x_velocity, y_velocity = gameOver(pausing, x_velocity, y_velocity)
-        # write(100, 100, str(dinosaur_rect), font)
-        # write(50, 50, str(obj_rect), font)
 
         if pausing and bool_s2:
             mixer.Sound.play(sound2)
             bool_s2 = False
-            # Khúc này để âm thanh chỉ xuất hiện 1 lần
 
         for event in get():
             if event.type == QUIT:
